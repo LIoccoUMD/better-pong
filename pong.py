099f10bcc96d24191d029db210c112818770b844
LIoccoUMD <liocco@umd.edu>
2025-04-04 23:50:26 -0400

    Restricted player movement to remain in the screen limitations. next add reset ball functionality and spacebar to play again

--- pong.py
+++ pong.py
@@ -1,8 +1,7 @@
 import pygame
 import random
 
-def inverse(num):
-    return -num
+BALL_RADIUS = 10
 
 pygame.init()
 screen = pygame.display.set_mode((1920,1080))
@@ -26,39 +25,43 @@ while running:
 
     screen.fill("blue")
 
-    # Paddle movement (fill this in)
+    # Player movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player1.y -= 10
+        player1.y -= 6
     if keys[pygame.K_s]:
-        player1.y += 10
+        player1.y += 6
     if keys[pygame.K_UP]:
-        player2.y -= 10
+        player2.y -= 6
     if keys[pygame.K_DOWN]:
-        player2.y += 10
+        player2.y += 6
+    player1.clamp_ip(screen.get_rect())
+    player2.clamp_ip(screen.get_rect())
     # Player 1: W/S
     # Player 2: UP/DOWN
 
-    # Ball movement (fill this in)
+    # Ball movement
     # Update ball_pos, check top/bottom bounce, reset if off left/right
     # Move the ball
+    if ball_pos.y > 1080 - BALL_RADIUS or ball_pos.y < BALL_RADIUS:
+        ball_speed_y = -ball_speed_y
     if ball_pos.x < 5 or ball_pos.x > 1915:
         ball_speed_x = -ball_speed_x
-    if ball_pos.y > 1075 or ball_pos.y < 5:
-        ball_speed_y = -ball_speed_y
     ball_pos.x += ball_speed_x * dt
     ball_pos.y += ball_speed_y * dt
     # Ball hitbox
-    ball_hitbox = pygame.Rect(ball_pos.x - 10, ball_pos.y - 10, 20, 20)  # 20x20 bounding box
+    ball_hitbox = pygame.Rect(ball_pos.x - BALL_RADIUS, ball_pos.y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)  # 20x20 bounding box
     if ball_hitbox.colliderect(player1) or ball_hitbox.colliderect(player2):
+        BALL_RADIUS += 1
+        # increase ball size each hit
         ball_speed_x = -ball_speed_x
     
     # Render
-    pygame.draw.circle(screen, "black", ball_pos, 10)
+    pygame.draw.circle(screen, "black", ball_pos, BALL_RADIUS)
     pygame.draw.rect(screen, "black", player1)
     pygame.draw.rect(screen, "black", player2)
 
     pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(144) / 1000
 
 pygame.quit()
