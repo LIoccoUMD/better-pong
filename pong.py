09a0c6ebcc08605cb972b07b2f5736b60049079c
LIoccoUMD <liocco@umd.edu>
2025-04-05 11:16:49 -0400

    Rotate paddles feature. update scope of paddleX_rotated

--- pong.py
+++ pong.py
@@ -1,8 +1,14 @@
 import pygame
 import random
 
+# Constants
 BALL_RADIUS = 10
+PADDLE_WIDTH = 20
+PADDLE_HEIGHT = 100
+PADDLE1_COLOR = (168, 127, 50)
+PADDLE2_COLOR = (106, 86, 168)
 
+# Initialize the game
 pygame.init()
 screen = pygame.display.set_mode((1920,1080))
 clock = pygame.time.Clock()
@@ -14,10 +20,21 @@ ball_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 ball_speed_x = random.choice([250, 500])
 ball_speed_y = random.randint(-250, 500)
 
-# Paddle setup (Rect objects)
-player1 = pygame.Rect(50, screen.get_height() / 2 - 50, 20, 100)  # Left paddle
-player2 = pygame.Rect(1870, screen.get_height() / 2 - 50, 20, 100)  # Right paddle
+# Unrotated base paddle
+paddle1_base = pygame.Surface((PADDLE_WIDTH,PADDLE_HEIGHT))
+paddle1_base.fill(PADDLE1_COLOR)
+paddle2_base = pygame.Surface((PADDLE_WIDTH,PADDLE_HEIGHT))
+paddle2_base.fill(PADDLE2_COLOR)
 
+# Rects to position -- easier to deal with collisions then to strictly use Surface
+player1 = pygame.Rect(50, screen.get_height() / 2 - 50, PADDLE_WIDTH, PADDLE_HEIGHT)  # Left paddle
+player2 = pygame.Rect(1870, screen.get_height() / 2 - 50, PADDLE_WIDTH, PADDLE_HEIGHT)  # Right paddle
+
+# Rotation angles
+paddle1_angle = 0
+paddle2_angle = 0
+
+# Game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,10 +68,24 @@ while running:
     ball_pos.y += ball_speed_y * dt
     # Ball hitbox
     ball_hitbox = pygame.Rect(ball_pos.x - BALL_RADIUS, ball_pos.y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)  # 20x20 bounding box
-    if ball_hitbox.colliderect(player1) or ball_hitbox.colliderect(player2):
-        BALL_RADIUS += 1
-        # increase ball size each hit
+    if ball_hitbox.colliderect(player1):
         ball_speed_x = -ball_speed_x
+        paddle1_angle += 30
+        paddle1_rotated = pygame.transform.rotate(paddle1_base, paddle1_angle)
+    if ball_hitbox.colliderect(player2):
+        ball_speed_x = -ball_speed_x
+        paddle2_angle += 30
+        paddle2_rotated = pygame.transform.rotate(paddle2_base, paddle2_angle)
+    paddle1_rect = paddle1_rotated.get_rect(center=player1.center)
+    paddle2_rect = paddle2_rotated.get_rect(center=player2.center)
+
+
+    # Draw rotated paddles
+    screen.blit(paddle1_rotated, paddle1_rect.topleft)
+    screen.blit(paddle2_rotated, paddle2_rect.topleft)
+        # BALL_RADIUS += 1
+        # increase ball size each hit
+
     
     # Render
     pygame.draw.circle(screen, "black", ball_pos, BALL_RADIUS)
